a77e70e17631a670c095fddd2290128127260024
shoaib <shoaibshazid@gmail.com>
2024-05-22 20:01:25 +0530

    implemented grpc communication between the services and added middleware logging.

--- types/types.go
+++ types/types.go
@@ -1,5 +1,17 @@
 package types
 
+type Invoice struct {
+	OBUID         int     `json:"obuID"`
+	TotalDistance float64 `json:"totalDistance"`
+	TotalAmount   float64 `json:"totalAmount"`
+}
+
+type Distance struct {
+	Value float64 `json:"value"`
+	OBUID int     `json:"obuID"`
+	Unix  int64   `json:"unix"`
+}
+
 type OBUData struct {
 	OBUID     int     `json:"obuID"`
 	Lat       float64 `json:"lat"`
